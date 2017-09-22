c3f3fd86ebceaa203d339220f0d781e84d0a2cff
Peter Strawn <pstrawn@stfrancishs.org>
2017-09-22 13:21:18 -0700

    Initial sharing of project

---
+++ MyGui.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class MyGui here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import javax.swing.*;
+
+public class MyGui
+{
+    public static void main(String args[])
+    {
+        JFrame frame = new JFrame("Happy Coding");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
